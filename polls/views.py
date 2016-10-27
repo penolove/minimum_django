012ce2d3ed8819b4aa69352965d3de10941e3197
longdick <penolove15@gmail.com>
2016-10-28 01:16:54 +0800

    add tourial

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'polls/index.html')
+    #return render(request, 'polls/index.html')
+    return render(request, 'bootstrap/tutorial/DynamicTabs.html')
